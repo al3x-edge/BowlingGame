6fe7ce74f7668573f713160bb0b093a0c8f892ca
Alexander Ambrose <alexa@fh-group.com>
2017-04-23 15:32:02 -0400

    Handle basic game mechanics
    All game mechanics of balls thrown, progression, spares, and strikes
    have been implemented.

--- lib/game.rb
+++ lib/game.rb
@@ -2,35 +2,63 @@ class Game
   def initialize
     # Initially game score is set to 0
     @score = 0
-    # Game starts with a frame of 1
-    @current_frame = 1
-    # Each frame starts with ball 1 and progresses to 2
-    @current_ball = 1
-
-    @total_balls = 0
-
-    @frames = [[0,0]]
+    @throws = Array.new
+    @total_throws = 0
   end
 
   def score
-    @frames.inject(0){ |sum, frame| sum + frame[0] + frame[1] }
+    score = 0
+
+    for frame in 0..(@throws.length - 1)
+      frameThrow = frame * 2
+      if strike?(frameThrow)
+        score += strike_score(frameThrow)
+      elsif spare?(frameThrow)
+        score += spare_score(frameThrow)
+      else
+        score += throw_score(frameThrow) + throw_score(frameThrow + 1)
+      end
+    end
+
+    score
   end
 
   def current_frame
-    @current_frame
+    (@throws.length / 2) + 1
   end
 
   def current_ball
-    (@total_balls % 2) + 1
+    (@total_throws % 2) + 1
   end
 
   # Throw ball for a current frame
   def throw_ball(score=0)
-    @frames[current_frame - 1][current_ball - 1] = score
     @score += score
-    @total_balls += 1
-    @current_frame += 1
+    @total_throws += 1
+    @throws << score
   end
 
+  private
+  def strike?(index)
+    throw_score(index) == 10
+  end
 
+  def spare?(index)
+    throw_score(index) + throw_score(index+1) == 10
+  end
+
+  def throw_score(index)
+    return 0 if @throws[index].nil?
+    return @throws[index]
+  end
+
+  # Return original 10 points and then combine points of next two ball rolls
+  def strike_score(index)
+    return 10 + throw_score(index+2) + throw_score(index+3)
+  end
+
+  # Return original 10 points and then combine points of next ball roll
+  def spare_score(index)
+    return 10 + throw_score(index+2)
+  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,9 +1,9 @@
 require_relative './spec_helper'
 
 describe Game do
-  context "start of game" do
-    let(:game) { Game.new }
+  let(:game) { Game.new }
 
+  context "start of game" do
     it "has a score of 0 before the first ball has been rolled" do
       expect(game.score).to eq 0
     end
@@ -17,26 +17,76 @@ describe Game do
     end
   end
 
-  context "frame progression" do
-    let(:game) { Game.new }
-
-    it "allows balls to be thrown for a score" do
+  context "one ball thrown with a score of 0" do
+    it "has a score of 0" do
       game.throw_ball(0)
       expect(game.score).to eq 0
     end
 
-    it "notes balls thrown of 2 after throwing first ball" do
+    it "has a current frame of 1" do
+      game.throw_ball(0)
+      expect(game.current_frame).to eq 1
+    end
+
+    it "has a current ball of 2" do
       game.throw_ball(0)
       expect(game.current_ball).to eq 2
     end
+  end
 
-    it "allow two balls to be thrown with a current score of 5" do
-      game.throw_ball(0)
-      game.throw_ball(5)
-
-      expect(game.current_ball).to eq 1
-      expect(game.current_frame).to eq 2
+  context "two balls thrown scoring 0 and 5" do
+    it "has a score of 5" do
+      roll_array(game, [0,5])
       expect(game.score).to eq 5
+    end
+
+    it "has a current frame of 2" do
+      roll_array(game, [0,5])
+      expect(game.current_frame).to eq 2
+    end
+
+    it "has a current ball of 1" do
+      roll_array(game, [0,5])
+      expect(game.current_ball).to eq 1
+    end
+  end
+
+  context "four balls thrown scoring 0,5,0 and 3" do
+    it "has a score of 8" do
+      roll_many(game, 4, 2)
+      expect(game.score).to eq 8
+    end
+
+    it "has a current frame of 3" do
+      roll_many(game, 4, 2)
+      expect(game.current_frame).to eq 3
+    end
+
+    it "has a current ball of 1" do
+      roll_many(game, 4, 2)
+      expect(game.current_ball).to eq 1
+    end
+  end
+
+  it "has a score of 24 with a strike and two balls thrown of 3 and 4" do
+    roll_array(game, [10,0,3,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])
+    expect(game.score).to eq 24
+  end
+
+  it "has a score of 20 with a spare and two balls thrown of 3 and 4" do
+    roll_array(game, [5,5,3,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])
+    expect(game.score).to eq 20
+  end
+
+  def roll_many(game, times, score)
+    for _ in (1..times)
+      game.throw_ball(score)
+    end
+  end
+
+  def roll_array(game, array)
+    for score in array
+      game.throw_ball(score)
     end
   end
 end
